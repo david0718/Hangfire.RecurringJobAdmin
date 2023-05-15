dc644347df457fca49cfcec106287cd4cd0fc10d
于兵建 <jr_yujb@yonyou.com>
2023-05-15 11:12:21 +0800

    ford-tdms init

---
+++ ford-tdms/Hangfire.Tdms.Library/TdmsCafPartsInventory.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire.RecurringJobAdmin;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hangfire.Tdms.Library
+{
+    public class TdmsCafPartsInventory
+    {
+        [RecurringJob("*/1 * * * *", "default", RecurringJobId = "TDMS-UpdateInventoryQuantity")]
+        [DisableConcurrentlyJobExecution(nameof(UpdateCafpartsInventoryQuantity))]
+        [AutomaticRetry(Attempts = 3, OnAttemptsExceeded = AttemptsExceededAction.Fail)]
+        public async Task UpdateCafpartsInventoryQuantity()
+        {
+            await Task.Run(() => Console.WriteLine("Here we will check part inbound and outbound information, then calculate the inventory count."));
+        }
+    }
+}
---
+++ ford-tdms/Hangfire.Tdms/TestExecution.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire.RecurringJobAdmin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire.JobExtensions.DotNetCore.Tdms.Test
+{
+    public class TestExecutionJob
+    {
+
+
+
+        public void TestConsole()
+        {
+            Console.WriteLine("Testing Console");
+        }
+
+        //[DisableConcurrentlyJobExecution("CheckFileExists", 0, 10, "It is not allowed to perform multiple same tasks.", jobState: JobState.FailedState)]
+        //[RecurringJob("*/2 * * * *", "UTC", "default", RecurringJobId = "Check-File-Exists")]
+        //public void CheckFileExists()
+        //{
+        //    Console.WriteLine("Check File Exists");
+        //}
+    }
+
+
+
+}
